1e4b3df24240f7524d6836195170322a5ab7dba1
Willian dos Santos <williansantos465@hotmail.com>
2021-10-16 21:45:29 -0300

    Criando estrutura de pastas do projeto, adicionado o gitignore e testando um rota

---
+++ Conta/Controllers/ContaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Conta.Controllers
+{
+    [Route("V{version:ApiVersion}/contas")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class ContaController : ControllerBase
+    {
+        public ContaController()
+        {
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public string Get()
+        {
+            return "ok";
+        }
+    }
+}
